94706361987c5317a23e639781efc8cfb072bd5c
Ryo Nakagome <nakagome@tento-net.com>
2019-05-22 13:44:50 +0900

    保護者用のナビゲーション追加

--- tmp/head.php
+++ tmp/head.php
@@ -16,4 +16,6 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 </head>
