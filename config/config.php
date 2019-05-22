494064d3f4fb6d5f6a3b23eb3212dce55900a089
Ryo Nakagome <nakagome@tento-net.com>
2019-05-22 14:26:56 +0900

    ナビゲーションを切り替える

---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php
+    $db_host = 'localhost';
+    $db_user = 'root';
+    $db_pass = '';
+    $db_name = 'fms_iflab';
+
+    $db_link = mysqli_connect($db_host, $db_user, $db_pas, $db_name);
+?>
---
+++ module/modulenav.php
@@ -0,0 +1,15 @@
+<?php
+    if(isset($_SESSION['uid']) != '') {
+        require_once('../config/config.php');
+        $uid = $_SESSION['uid'];
+        $sql = mysqli_query($db_link, "SELECT Permission FROM User WHERE UserID = '$uid'");
+        $result = mysqli_fetch_assoc($sql);
+        if($result['Permission'] == 1) {
+            require_once('../tmp/navparents.php');
+        } else ($result['Permission'] == 2) {
+            require_once('../tmp/navteacher.php');
+        }
+    } else {
+        require_once('../tmp/nav.php');
+    }
+?>
